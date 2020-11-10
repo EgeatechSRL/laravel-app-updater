f22f8842db9dc0d98ff219cdf871f53d0ae50335
Guidolin Marco <m.guidolin@egeatech.com>
2020-11-10 18:04:20 +0100

    ADD: package settings

--- config/app-updater.php
+++ config/app-updater.php
@@ -1,4 +1,14 @@
 <?php
 
+use EgeaTech\AppUpdater\Models\Application;
+
 return [
+    'disk' => 'local',
+    'file_folder_name' => 'APK',
+    'migrations' => [
+        'applications_table_name' => 'applications',
+    ],
+    'models' => [
+        'application' => Application::class,
+    ]
 ];
