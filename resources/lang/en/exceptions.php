3b2f726c581d26575bf62f7cd742ca806e56f15f
Guidolin Marco <m.guidolin@egeatech.com>
2020-11-10 18:37:35 +0100

    FIX: language files

---
+++ resources/lang/en/exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'duplicated_version' => 'The version you want to assign is already in use for the current build channel'
+];
---
+++ resources/lang/it/exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'duplicated_version' => 'La versione che vuoi assegnare è già in uso per il canale di build corrente'
+];
