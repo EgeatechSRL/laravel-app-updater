1fcaa88678d8ab0a505979962081d9b59fc51aa4
Guidolin Marco <m.guidolin@egeatech.com>
2020-11-10 18:08:47 +0100

    ADD: packages routes registration helper

--- src/AppUpdater.php
+++ src/AppUpdater.php
@@ -2,6 +2,33 @@
 
 namespace EgeaTech\AppUpdater;
 
+use Illuminate\Support\Facades\Route;
+use EgeaTech\AppUpdater\ValueObjects\ApplicationId;
+use EgeaTech\AppUpdater\Http\Routing\RouteRegistrar;
+use EgeaTech\AppUpdater\Http\Routing\RoutingOptions;
+
 class AppUpdater
 {
+    /**
+     * Registers all available package routes
+     *
+     * @param null|RoutingOptions $options
+     * @return void
+     */
+    public static function routes(?RoutingOptions $options = null): void
+    {
+        if (!$options) {
+            $options = new RoutingOptions();
+        }
+
+        // Register the new parameter binding
+        Route::bind('applicationId', function (string $value): ApplicationId {
+            return new ApplicationId($value);
+        });
+
+        // And all package routes
+        Route::group($options->getGlobalRoutesOptions(), function($router) use ($options) {
+            (new RouteRegistrar($router, $options))->all();
+        });
+    }
 }
