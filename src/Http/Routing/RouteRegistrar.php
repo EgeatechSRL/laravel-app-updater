4374d58e0de5957b9d9094fc4c17427d6cbc56e1
Guidolin Marco <m.guidolin@egeatech.com>
2020-11-10 18:08:18 +0100

    ADD: package APIs routes and their available options

---
+++ src/Http/Routing/RouteRegistrar.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace EgeaTech\AppUpdater\Http\Routing;
+
+use Illuminate\Contracts\Routing\Registrar as Router;
+use EgeaTech\AppUpdater\Http\Controllers\{
+    ApplicationIndexController,
+    ApplicationStoreController,
+    LatestApplicationController,
+    ApplicationUpdateController,
+    DownloadApplicationFileController,
+    ApplicationDeleteController
+};
+
+class RouteRegistrar
+{
+    protected $router;
+    protected $routeOptions;
+
+    public function __construct(Router $router, RoutingOptions $routeOptions)
+    {
+        $this->router = $router;
+        $this->routeOptions = $routeOptions;
+    }
+
+    /**
+     * Register package routes
+     *
+     * @return void
+     */
+    public function all()
+    {
+        $this->router
+            ->group([], function(Router $router) {
+
+                $router->get(
+                    '/applications',
+                    [
+                        'uses' => ApplicationIndexController::class,
+                        'as' => 'app-updater.application-index',
+                    ]
+                )
+                ->middleware($this->routeOptions->getIndexRouteMiddlewares());
+
+                $router->get(
+                    '/applications/latest',
+                    [
+                        'uses' => LatestApplicationController::class,
+                        'as' => 'app-updater.latest-application',
+                    ]
+                )
+                ->middleware($this->routeOptions->getLatestApplicationRouteMiddlewares());
+
+                $router->delete(
+                    '/applications/{applicationId}',
+                    [
+                        'uses' => ApplicationDeleteController::class,
+                        'as' => 'app-updater.application-delete',
+                    ]
+                )
+                ->middleware($this->routeOptions->getDeleteRouteMiddlewares());
+
+                $router->post(
+                    '/applications',
+                    [
+                        'uses' => ApplicationStoreController::class,
+                        'as' => 'app-updater.application-store',
+                    ]
+                )
+                ->middleware($this->routeOptions->getStoreRouteMiddlewares());
+
+                $router->patch(
+                    '/applications/{applicationId}',
+                    [
+                        'uses' => ApplicationUpdateController::class,
+                        'as' => 'app-updater.application-update',
+                    ]
+                )
+                ->middleware($this->routeOptions->getUpdateRouteMiddlewares());
+
+                $router->get(
+                    '/applications/{applicationId}/download',
+                    [
+                        'uses' => DownloadApplicationFileController::class,
+                        'as' => 'app-updater.application-download',
+                    ]
+                )
+                ->middleware($this->routeOptions->getDownloadApplicationRouteMiddlewares());
+
+            });
+    }
+}
---
+++ src/Http/Routing/RoutingOptions.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace EgeaTech\AppUpdater\Http\Routing;
+
+final class RoutingOptions
+{
+    private $_globalRoutesOptions;
+    private $_indexRouteMiddlewares;
+    private $_storeRouteMiddlewares;
+    private $_updateRouteMiddlewares;
+    private $_deleteRouteMiddlewares;
+    private $_latestApplicationRouteMiddlewares;
+    private $_downloadApplicationRouteMiddlewares;
+
+    public function __construct(
+        array $globalPackageRoutesOptions = [],
+        array $indexRouteMiddlewares = [],
+        array $storeRouteMiddlewares = [],
+        array $updateRouteMiddlewares = [],
+        array $deleteRouteMiddlewares = [],
+        array $latestApplicationRouteMiddlewares = [],
+        array $downloadApplicationRouteMiddlewares = []
+    ) {
+        $this->_globalRoutesOptions = $globalPackageRoutesOptions;
+        $this->_indexRouteMiddlewares = $indexRouteMiddlewares;
+        $this->_storeRouteMiddlewares = $storeRouteMiddlewares;
+        $this->_updateRouteMiddlewares = $updateRouteMiddlewares;
+        $this->_deleteRouteMiddlewares = $deleteRouteMiddlewares;
+        $this->_latestApplicationRouteMiddlewares = $latestApplicationRouteMiddlewares;
+        $this->_downloadApplicationRouteMiddlewares = $downloadApplicationRouteMiddlewares;
+    }
+
+    public function getGlobalRoutesOptions(): array
+    {
+        return $this->_globalRoutesOptions;
+    }
+
+    public function getIndexRouteMiddlewares(): array
+    {
+        return $this->_indexRouteMiddlewares;
+    }
+
+    public function getStoreRouteMiddlewares(): array
+    {
+        return $this->_storeRouteMiddlewares;
+    }
+
+    public function getUpdateRouteMiddlewares(): array
+    {
+        return $this->_updateRouteMiddlewares;
+    }
+
+    public function getDeleteRouteMiddlewares(): array
+    {
+        return $this->_deleteRouteMiddlewares;
+    }
+
+    public function getLatestApplicationRouteMiddlewares(): array
+    {
+        return $this->_latestApplicationRouteMiddlewares;
+    }
+
+    public function getDownloadApplicationRouteMiddlewares(): array
+    {
+        return $this->_downloadApplicationRouteMiddlewares;
+    }
+}
