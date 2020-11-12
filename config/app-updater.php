63fa2f2b43c14a4b1dd530fb10ba3ea44d4ff8bd
Guidolin Marco <m.guidolin@egeatech.com>
2020-11-12 18:28:51 +0100

    ADD: configuration keys information

--- config/app-updater.php
+++ config/app-updater.php
@@ -1,14 +1,38 @@
 <?php
 
-use EgeaTech\AppUpdater\Models\Application;
-
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | The disk where to store uploaded applications installers. The value must
+    | refer to an existing
+    |
+    */
     'disk' => 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | The name of the folder where all application installers will be stored.
+    |
+    */
     'file_folder_name' => 'APK',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Migrations settings
+    |--------------------------------------------------------------------------
+    |
+    | Here you can define the name of the table that will be created by the
+    | package when running the migration file.
+    |
+    */
     'migrations' => [
         'applications_table_name' => 'applications',
     ],
-    'models' => [
-        'application' => Application::class,
-    ]
 ];
